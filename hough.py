e9d2cbca0d2c5fa9923a370d9331a4cd7712f68f
HoVanCon <hovancon1998@gmail.com>
2019-03-15 22:59:32 +0700

    Determine angle of image

---
+++ hough.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import numpy as np
+import math
+
+src = cv.imread('image/example_03.jpeg',0)
+dst = cv.bitwise_not(src)
+w, h= np.array(src.shape)
+lines = cv.HoughLinesP(dst,1,np.pi/180,100,w/2,20)
+size = np.array(lines.shape)
+
+angle = 0;
+
+if lines is not None:
+	for i in range(0,len(lines)):
+		l = lines[i][0]
+		angle = angle + math.atan2(l[3]-l[1],l[2]-l[0])
+		cv.line(src, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv.LINE_AA)
+
+angle = angle*180/np.pi
+print(angle/len(lines))
+cv.imshow('src',src)
+cv.imshow('test',dst)
+cv.waitKey()
